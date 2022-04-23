41fdf6ce1da87bb804508fcc51bd64d811b68107
Eliecer Diaz <eliecerdiazrojas@gmail.com>
2022-04-23 12:05:24 -0400

    just an effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,28 @@
 $(function () {
 
     $("a").click(function (event) {
-
         if (this.hash !== "") {
             event.preventDefault();
-
             var gato = this.hash;
-
             $("html, body").animate({
                 scrollTop: $(gato).offset().top
             }, 800);
         }
     });
 
-
-
-   
-
 });
 
-$(document).scroll( function(e){
+$(document).scroll(function (e) {
     const y = $("html").scrollTop();
-    if(y > 300) $("nav").addClass("nav-black")
-    else $("nav").removeClass("nav-black")
+    if (y > 300) $("nav").addClass("nav-black")
+    else $("nav").removeClass("nav-black");
+
+    if (y > 300) {        
+        $( ".card img" ).removeClass("d-none");
+        $( ".card" ).fadeIn( 3000, function() {            
+            $( "img" ).fadeIn( 1000);
+        });
+    };
 })
 
 
-
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
